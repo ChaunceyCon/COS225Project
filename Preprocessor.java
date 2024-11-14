3e16609c73ff361f4c380947f9079fb32107a17d
stephenm05 <stephen.mccollum@maine.edu>
2024-11-14 11:20:11 -0500

    Created skeleton of Preprocessor class

---
+++ Preprocessor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Preprocessor {
+    
+    public void keepLongest(String readFile,String writeFile)
+    {
+
+    }
+
+    public void decapitalize(String readFile,String writeFile)
+    {
+        
+    }
+
+}
