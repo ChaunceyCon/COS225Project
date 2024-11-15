41fdd3634f7ded0a75f9dc89a9827d1b0d9ec523
kateamartell <kate.martell@maine.edu>
2024-11-15 16:38:05 -0500

    Create.java

--- Create.java
+++ Create.java
@@ -13,24 +13,11 @@ public class Create {
 
         try (MongoClient mongoclient = MongoClients.create(connectionString)) {
 
-            // Connect to the desired database
+            
             MongoDatabase database = mongoclient.getDatabase("story_database");  
 
-            // Connect to the stories collection (this collection will store the short stories)
             MongoCollection<Document> storiesCollection = database.getCollection("stories");
 
-            // Creating example story documents
-            Document firstStory = new Document();
-            firstStory.append("story_text", "Once upon a time, there was a little girl who was very happy. She had a good family and friends. One day, she went to the park and played with her dog. She laughed and enjoyed her time. The day was perfect.")
-                      .append("tone", "Optimistic");
-
-            Document secondStory = new Document();
-            secondStory.append("story_text", "A boy woke up to a rainy day. He had no friends and always felt lonely. He stayed inside, reading books to pass time. He wanted to go outside but feared the world. His life seemed dull.")
-                       .append("tone", "Pessimistic");
-
-            Document thirdStory = new Document();
-            thirdStory.append("story_text", "There was a traveler who had a long journey ahead. He felt tired but kept walking. The road was tough, but he kept moving forward. Eventually, he saw a beautiful sunset. He felt proud of his progress.")
-                       .append("tone", "Optimistic");
 
             // Inserting stories into the MongoDB collection
             storiesCollection.insertOne(firstStory);
