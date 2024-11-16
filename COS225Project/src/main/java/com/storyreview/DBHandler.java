a3e1425182fbf34dc4e1c9237abd9dd533d7dce0
ChaunceyCon <chauncey.oconnell@maine.edu>
2024-11-15 23:25:35 -0500

    Final Commits and backend almost complete

--- COS225Project/src/main/java/com/storyreview/DBHandler.java
+++ COS225Project/src/main/java/com/storyreview/DBHandler.java
@@ -13,27 +13,36 @@ import static com.mongodb.client.model.Filters.*;
 
 public class DBHandler {
 
+    MongoClient client;
     MongoDatabase database;
     MongoCollection<Document> storiesCollection;
     
     public DBHandler(String conString)
     {
-        try(MongoClient client = MongoClients.create(conString))
-        {
-            database = client.getDatabase("admin");
-            storiesCollection = database.getCollection("stories");
-        }
+        client = MongoClients.create(conString);
+        database = client.getDatabase("admin");
+        storiesCollection = database.getCollection("stories");
     }
     
     public void addStory(String line)
     {
         Scanner parser = new Scanner(line);
         parser.useDelimiter(";;");
-        String title = parser.next();
-        String story = parser.next();
-        String labels = parser.next();
-        Document targetStory = new Document();
-        targetStory.append("title",title).append("story",story).append("labels",labels);
-        storiesCollection.insertOne(targetStory);
+        try {
+            String title = parser.next();
+            String story = parser.next();
+            String labels = parser.next();
+            Document targetStory = new Document();
+            targetStory.append("title", title).append("story", story).append("labels", labels);
+            storiesCollection.insertOne(targetStory);
+        } finally {
+            parser.close();
+        }
+    }
+
+    public void close() {
+        if (client != null) {
+            client.close();
+        }
     }
 }
