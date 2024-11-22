46b00dc06034fb5e65269de3bdcbc5dbeb17d43c
stephenm05 <stephen.mccollum@maine.edu>
2024-11-22 17:19:31 -0500

    Updated Shutdown.reset() to clear the files Preprocessor writes to

--- COS225Project/src/main/java/com/storyreview/story/Shutdown.java
+++ COS225Project/src/main/java/com/storyreview/story/Shutdown.java
@@ -5,6 +5,23 @@ import java.io.*;
 public class Shutdown {
     
     public static void reset(String shortPath,String finPath) {
-
+        File shortest = new File(shortPath);
+        File fin = new File(finPath);
+        //clears the file Preprocessor.keepLongest() writes to by deleting and re-creating it
+        shortest.delete();
+        try {
+            shortest.createNewFile();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("Error clearing "+shortPath);
+        }
+        //clears the file Preprocessor.processFile() writes to
+        fin.delete();
+        try {
+            fin.createNewFile();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("Error clearing "+finPath);
+        }
     }
 }
