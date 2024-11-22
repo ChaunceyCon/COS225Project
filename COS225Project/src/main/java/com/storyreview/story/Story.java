04e1c9cde2e332bd5f456ab93c410f81c999302e
stephenm05 <stephen.mccollum@maine.edu>
2024-11-22 12:25:53 -0500

    Added skeleton of constructor

--- COS225Project/src/main/java/com/storyreview/story/Story.java
+++ COS225Project/src/main/java/com/storyreview/story/Story.java
@@ -1,5 +1,22 @@
 package com.storyreview.story;
 
+import java.util.*;
+
 public class Story {
     
+    private String title,iniStory,finStory,sentiment;
+    private ArrayList<String> emotions = new ArrayList<String>();
+
+    public Story(String t,String iS,String fS,String labels) {
+        title=t;
+        iniStory=iS;
+        finStory=fS;
+        //fill emotions by parsing labels
+        Scanner lScanner = new Scanner(labels);
+        lScanner.useDelimiter("/");
+        while(lScanner.hasNext()) {
+            emotions.add(lScanner.next());
+        }
+        //determine sentiment by seeing whether the emotions are primarily positive or negative (need to categorize all the emotions first)
+    }
 }
