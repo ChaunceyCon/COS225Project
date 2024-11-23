d0e3aa2774b42d4a4b6c537b5e83cae794beedd2
stephenm05 <stephen.mccollum@maine.edu>
2024-11-23 02:14:22 -0500

    Updated Startup to reflect changes to DBHandler

--- COS225Project/src/main/java/com/storyreview/story/Startup.java
+++ COS225Project/src/main/java/com/storyreview/story/Startup.java
@@ -2,10 +2,10 @@ package com.storyreview.story;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 import com.storyreview.database.*;
-import com.storyreview.menu.*;
+//import com.storyreview.menu.*;
 import com.storyreview.mlp.TFIDF;
 
 public class Startup {
@@ -17,11 +17,15 @@ public class Startup {
         String shortPath = "src/resources/short.txt";
         String finPath = "src/resources/final.txt";
         
+        //reset everything to defaults after last run. will probably move this to the end before the actual presentation
+        Shutdown.reset(shortPath,finPath);
+
         //create a TFIDF object to effeciently get the necessary MLP data during processing
         TFIDF storyProcessor = new TFIDF();
         //fill final.txt by processing raw.txt with Preprocesser
         Preprocessor.keepLongest(rawPath,shortPath);
         Preprocessor.processFile(shortPath,finPath,storyProcessor);
+        HashMap<String,Story> storyCollection = Preprocessor.getStoryCollection();
         
         try {
             // Get connection string from file and create DB Handler
@@ -30,8 +34,13 @@ public class Startup {
             DBHandler dbHandler = new DBHandler(conString,"stories");
             csScanner.close();
 
-            // Upload data from final.txt
-            dbHandler.uploadFromFile("src/resources/final.txt");
+            // Upload data from storyCollection
+            int storiesAdded=0;
+            for(Story s : storyCollection.values()) {
+                dbHandler.addStory(s);
+                storiesAdded++;
+            }
+            System.out.println("Added "+storiesAdded+" stories to MongoDB");
             
             // Close the connection
             dbHandler.close();
@@ -41,7 +50,5 @@ public class Startup {
             e.printStackTrace();
             System.out.println("Error finding the connection string file!");
         }
-
-        //Shutdown.reset(shortPath,finPath);
     }
 }
