f2a32a72685e1e077a16aec121cfda45e55ae5f4
kateamartell <kate.martell@maine.edu>
2024-12-03 13:51:37 -0500

    Imports
    Resourced from Github movie app.
    Comments at bottom are what we are trying to implement.

--- COS225Project/src/main/java/com/storyreview/menu/Menu.java
+++ COS225Project/src/main/java/com/storyreview/menu/Menu.java
@@ -1,13 +1,34 @@
 package com.storyreview.menu;
 
-import com.storyreview.mlp.*;
+import com.storyreview.database.Database;
+import com.storyreview.mlp.TFIDF;
+import com.storyreview.mlp.Classifier;
+import com.storyreview.mlp.Processor;
+import com.storyreview.story.Story;
+import com.storyreview.story.StoryReview;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.bson.BsonValue;
+import com.mongodb.client.result.InsertOneResult;
 
 public class Menu {
-    
-    public static void runMenu(TFIDF TFIDFProcessor,Classifier classifier) {
+
+
+   
+    }
+
+
+
+        
+        
         //run a while loop until the user chooses to exit
         //each iteration, display the menu options and ask the user for a number representing their choice
         //use a switch statement to use the appropriate feature based on what the user selects
         //for the main mlp feature you just need to get another user input for the user's story and then run classifier.classifyUserStory(String userStoryThatYouJustGot,TFIDFProcessor); and it should work
-    }
-}
+
+
