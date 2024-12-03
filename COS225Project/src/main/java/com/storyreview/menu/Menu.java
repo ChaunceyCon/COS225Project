1678d6de9197efc3d628bf2e92237c0e6d058087
stephenm05 <stephen.mccollum@maine.edu>
2024-12-03 11:52:19 -0500

    Added comments clarifying how classifyUserStory() works

--- COS225Project/src/main/java/com/storyreview/menu/Menu.java
+++ COS225Project/src/main/java/com/storyreview/menu/Menu.java
@@ -1,5 +1,13 @@
 package com.storyreview.menu;
 
+import com.storyreview.mlp.*;
+
 public class Menu {
     
+    public static void runMenu(TFIDF TFIDFProcessor,Classifier classifier) {
+        //run a while loop until the user chooses to exit
+        //each iteration, display the menu options and ask the user for a number representing their choice
+        //use a switch statement to use the appropriate feature based on what the user selects
+        //for the main mlp feature you just need to get another user input for the user's story and then run classifier.classifyUserStory(String userStoryThatYouJustGot,TFIDFProcessor); and it should work
+    }
 }
