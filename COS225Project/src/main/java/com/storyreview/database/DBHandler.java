f63443cadcd9c6787034a4f3659dcd47e711b7d2
kateamartell <kate.martell@maine.edu>
2024-12-13 13:43:23 -0500

    added a limitation factor
    added .limit(1) so it only gives the user one story based on what sentiment or emotion they provided.

--- COS225Project/src/main/java/com/storyreview/database/DBHandler.java
+++ COS225Project/src/main/java/com/storyreview/database/DBHandler.java
@@ -57,7 +57,7 @@ public class DBHandler {
         List<String> stories = new ArrayList<>();
 
         String regex = "\\b" + sentiment + "\\b";
-        MongoCursor<Document> cursor = storiesCollection.find(Filters.regex("story", regex, "i")).iterator();
+        MongoCursor<Document> cursor = storiesCollection.find(Filters.regex("story", regex, "i")).limit(1).iterator(); 
         
         while (cursor.hasNext()) {
             Document doc = cursor.next();
